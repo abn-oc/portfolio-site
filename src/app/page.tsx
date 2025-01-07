266b56dbea36bd633cb3d47473d73fa92b109746
abn-oc <abdullah.iht22@gmail.com>
2025-01-07 21:07:49 +0500

    fetched leetcode github stats

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+import { Octokit } from "octokit"
+
+export default async function Land() {
+
+  //getting leetcode profile stats
+  const lcURL = "https://alfa-leetcode-api.onrender.com/userProfile/abnoc"
+  const lcDetails = await (await fetch(lcURL)).json()
+  console.log(lcDetails)
+  //getting leetcode language stats
+  const langURL = "https://alfa-leetcode-api.onrender.com/languageStats?username=abnoc"
+  const langDetails = await (await fetch(langURL)).json()
+  console.log(langDetails)
+  //getting github commits
+  const octokit = new Octokit({
+    auth: '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'
+  })
+  const ghDetails = await octokit.request('GET /users/{username}/events', {
+    username: "abn-oc"
+  })
+  console.log(ghDetails)
+
+  return (
+    <div className="p-12">
+    <h1 className="text-3xl">leet API</h1>
+    <p>hi</p>
+    </div>
+  )
+}
