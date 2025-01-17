92f536f82c094d82739ec949c782b2f6e88807e3
abn-oc <abdullah.iht22@gmail.com>
2025-01-17 09:30:59 +0500

    restarted landing

---
+++ src/app/_components/nav.tsx
@@ -0,0 +1,12 @@
+export default function Nav() {
+
+    return (
+        <div className="flex flex-row text-zinc-400 gap-4 text-xl mt-2 py-2">
+            <a href="" className="text-zinc-50">abdullah iht</a>
+            <div className="m-auto"></div>
+            <a href="">stats</a>
+            <a href="">projects</a>
+            <a href="">experience</a>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import AboutMe from "./_components/about_me";
 import Landing from "./_components/landing";
+import Nav from "./_components/nav";
 
 export default function Main() {
   return (
     <div className="bg-zinc-900 px-8 sm:px-12 overflow-x-hidden">
+    {/* <Nav/> */}
     <Landing/>
-    <AboutMe/>
+    <Landing/>
+    {/* <Landing/> */}
     </div>
   )
 }
